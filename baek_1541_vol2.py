34a6912013711a2edbe2ed7bc9f4a5b0cbe01ab7
gw <bgw6051@naver.com>
2022-12-22 22:50:26 +0900

    greedy

---
+++ baek_1541_vol2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 22 22:35:44 2022
+
+@author: gw
+"""
+
+'''
+세준이는 양수와 +, -, 그리고 괄호를 가지고 식을 만들었다. 그리고 나서 세준이는 괄호를 모두 지웠다.
+
+그리고 나서 세준이는 괄호를 적절히 쳐서 이 식의 값을 최소로 만들려고 한다.
+
+괄호를 적절히 쳐서 이 식의 값을 최소로 만드는 프로그램을 작성하시오.
+'''
+
+a = input().split('-')
+num = []
+for i in a:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
