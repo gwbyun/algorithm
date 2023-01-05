95988c2cf66c2c6f0aa434c5d42bcef647bd4b44
gw <bgw6051@naver.com>
2023-01-05 19:30:23 +0900

    implementation

---
+++ baek_2439.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 19:10:30 2023
+
+@author: gw
+"""
+
+n = int(input())
+
+for i in range (1,n+1):
+    print(' '*(n-i)+'*' * i)
