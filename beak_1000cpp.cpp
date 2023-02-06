978ca089374ce83b627e0c25c0cc7e789b6873eb
gw <bgw6051@naver.com>
2023-02-06 17:36:37 +0900

    cpp

---
+++ beak_1000cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+
+{
+	int A;
+	int B;
+
+	cin >> A;
+	cin >> B;
+
+	cout << A + B;
+	return 0;
+
+
+}
---
+++ beak_10869cpp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int A;
+	int B;
+
+	cin >> A >> B;
+	
+
+	cout << A + B <<"\n";
+	cout << A - B << "\n";
+	cout << A * B << "\n";
+	cout << A / B << "\n";
+	cout << A % B << "\n";
+
+	return 0;
+}
